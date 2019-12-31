f6e678259870e320750acb6bda2bb32311e0f51a
Kevin Raynel <kevinr@theodo.fr>
2019-12-31 10:56:45 +0100

    Add several assets locations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\BreedRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -18,12 +19,30 @@ class HomeController extends AbstractController
         $this->breedRepository = $breedRepository;
     }
 
-    public function homeAction(): Response
+    public function homeAction(Request $request): Response
     {
         $breeds = $this->breedRepository->findAll();
 
+        if ($request->query->has('relative_assets') && $request->query->get('relative_assets') === 'true') {
+            $assets = 'relative';
+        } else {
+            if ($request->query->has('cache_assets') && $request->query->get('cache_assets') === 'true') {
+                $cache = 'cache';
+            } else {
+                $cache = 'nocache';
+            }
+
+            if ($request->query->has('https_assets') && $request->query->get('https_assets') === 'true') {
+                $secure = 'secure';
+            } else {
+                $secure = 'insecure';
+            }
+            $assets = sprintf('cdn_%s_%s', $cache, $secure);
+        }
+
         return $this->render('home.html.twig', [
             'breeds' => $breeds,
+            'assets' => $assets,
         ]);
     }
 }
